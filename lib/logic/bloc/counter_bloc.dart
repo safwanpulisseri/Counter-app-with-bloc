d083baa009938939b8dcace47122db73d9b74eae
Safwan Pulisseri <safwanpulisseri123@gmail.com>
2024-04-29 15:18:20 +0530

    changed counter bloc method

--- lib/logic/bloc/counter_bloc.dart
+++ lib/logic/bloc/counter_bloc.dart
@@ -6,23 +6,33 @@ part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(CounterInitial()) {
-    on<CounterEvent>(
-      (event, emit) {
-        if (event is IncrementEvent) {
-          emit(
-            CounterState(
-              counterValue: state.counterValue + 1,
-            ),
-          );
-        }
-        if (event is DecrementEvent) {
-          emit(
-            CounterState(
-              counterValue: state.counterValue - 1,
-            ),
-          );
-        }
-      },
+    //   on<CounterEvent>(
+    //     (event, emit) {
+    //       if (event is IncrementEvent) {
+    //         emit(
+    //           CounterState(
+    //             counterValue: state.counterValue + 1,
+    //           ),
+    //         );
+    //       }
+    //       if (event is DecrementEvent) {
+    //         emit(
+    //           CounterState(
+    //             counterValue: state.counterValue - 1,
+    //           ),
+    //         );
+    //       }
+    //     },
+    //   );
+    on<IncrementEvent>(
+      (event, emit) => emit(
+        CounterState(counterValue: state.counterValue + 1),
+      ),
+    );
+    on<DecrementEvent>(
+      (event, emit) => emit(
+        CounterState(counterValue: state.counterValue - 1),
+      ),
     );
   }
 }
